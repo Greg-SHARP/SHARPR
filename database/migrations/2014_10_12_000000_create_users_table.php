197f0805c696809bad2833d2bc30e00574c70297
Carl Dawson <carl@invisible-gorilla.com>
2017-09-24 11:05:26 -0400

    mend

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->enum('status', ['inactive', 'active', 'blocked']);
-            $table->boolean('verified');
             $table->timestamps();
         });
     }
